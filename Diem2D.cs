09961bc1249f3a65eb80377498960e9396098097
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2024-01-08 22:27:27 +0700

    Add files via upload
    xd lớp phân số , lớp ko gian xy

---
+++ Diem2D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Diem2D
+    {
+        int x, y;
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double tinhkc()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+        public void nhap()
+        {
+            Console.WriteLine("Nhap toa do x:");
+            X = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap toa do y:");
+            Y = int.Parse(Console.ReadLine());
+        }
+        public void xuat()
+        {
+            Console.WriteLine("toa do x:"+x+"Toa do y"+y);
+            Console.WriteLine("toa do x:{0}  toa do y: {1}",x ,y);
+        }
+    }
+}
---
+++ phanso.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class phanso
+    {
+        int tu, mau;
+
+        public int Mau
+        {
+            get { return mau; }
+            set { mau = value; }
+        }
+
+        public int Tu
+        {
+            get { return tu; }
+            set { tu = value; }
+        }
+        public void nhap()
+        {
+            Console.WriteLine("NHap tu so:");
+            Tu=int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("NHap mau so:");
+                Mau = int.Parse(Console.ReadLine());
+            } while (Mau == 0);
+        }
+        public void xuat()
+        {
+            Console.WriteLine("{0}/{1} ",tu,mau);
+        }
+        public void chuyendau()
+        {
+            if(mau<0)
+            {
+                tu = tu * -1;
+                mau = mau * -1;
+            }
+        }
+        int USCLN(int a,int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while(a!=b)
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            return a;
+        }
+      
+        public void RGPS()
+        {
+            tu = tu / USCLN(tu, mau);
+            mau = mau / USCLN(tu, mau);
+        }
+
+    }
+}
