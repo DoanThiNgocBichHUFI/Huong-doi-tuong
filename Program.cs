ccac4ccf8950beeaad3b39d45ebaded931ca9b93
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2024-01-08 22:20:31 +0700

    Add files via upload
    xd lớp hình tròn , hình CN

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hinhTron a= new hinhTron();
+            //a.nhap();
+            //double b = a.tinhCV();
+            //Console.WriteLine("Chu vi hinh tron= {0}",b);
+            //a.xuat();
+            //Console.ReadLine();
+
+            //hinhCN a = new hinhCN();
+            //a.nhap();
+            //a.xuat();
+
+            // Tạo một đối tượng mới của lớp hinhCN
+            hinhCN hinh = new hinhCN();
+
+            // Gán giá trị cho chiều rộng và chiều dài
+            hinh.Cr = 10;
+
+            // In ra giá trị ban đầu của chiều rộng và chiều dài
+            Console.WriteLine("Chiều rộng ban đầu: " + hinh.Cr);
+
+            // Sử dụng phương thức changeSize để thay đổi kích thước
+            hinh.changeSize(2, 3, 1);
+
+            // In ra giá trị sau khi thay đổi kích thước
+            Console.WriteLine("Chiều rộng sau khi tăng: " + hinh.Cr);
+
+            // Sử dụng phương thức changeSize để thay đổi kích thước
+            hinh.changeSize(2, 3, 0);
+
+            // In ra giá trị sau khi thay đổi kích thước
+            Console.WriteLine("Chiều rộng sau khi giảm: " + hinh.Cr);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hinhTron.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi_2
+{
+    class hinhTron
+    {
+        private double bankinh;
+
+        public double BanKinh {
+            get {
+                return bankinh;
+            }
+            set {
+                if (value < 0)
+                {
+                    bankinh = 0;
+                }
+                else
+                    bankinh = value;
+            }
+        }
+
+        public hinhTron() {
+            this.bankinh = 0;
+        }
+
+        public hinhTron(double bankinh) {
+            this.BanKinh = bankinh;
+        }
+
+        public hinhTron(hinhTron a) {
+            this.bankinh = a.bankinh;
+        }
+
+        ~hinhTron() { 
+        
+        }
+
+        public double tinhCV() {
+            return this.bankinh * 3.14 * 2;
+        }
+
+        public double tinhDT() {
+            return this.bankinh * this.bankinh * 3.14;
+        }
+
+        public void nhap() {
+            Console.WriteLine("Nhap ban kinh: ");
+            this.BanKinh = double.Parse(Console.ReadLine());
+        }
+
+        public void xuat() {
+            Console.WriteLine("Ban kinh: {0}, chu vi: {1},dien tich: {2}",bankinh,tinhCV(),tinhDT());
+        }
+    }
+}
